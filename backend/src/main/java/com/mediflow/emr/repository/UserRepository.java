0725ede58325a325fea5b78323820dad053d231a
minee0505 <wodeyue97@gmail.com>
2025-11-22 00:12:15 +0900

    feat: UserRepository 인터페이스 추가
    - 사용자 이메일과 Provider ID로 조회 가능한 메서드 추가
    - JPA Repository 기반으로 사용자 데이터 액세스 구성

---
+++ backend/src/main/java/com/mediflow/emr/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mediflow.emr.repository;
+
+import com.mediflow.emr.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByProviderId(String providerId);
+}
